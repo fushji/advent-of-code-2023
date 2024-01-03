a67821243f4bad1faa1c55891c5f41bb9e57cd5f
a.perrone <a.perrone@reply.it>
2024-01-03 16:11:41 +0100

    Solution day 11

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,11 +1,340 @@
+use std::{
+    collections::HashSet,
+    ops::{Add, Div, Mul, Sub},
+};
+
+use itertools::Itertools;
+
 advent_of_code::solution!(10);
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+enum Pipe {
+    NorthSouth,
+    EastWest,
+    NorthEast,
+    NorthWest,
+    SouthWest,
+    SouthEast,
+    Ground,
+    Start,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Tile {
+    position: (usize, usize),
+    pipe: Pipe,
+}
+
+impl Tile {
+    fn new(position: (usize, usize), pipe: Pipe) -> Tile {
+        Tile { position, pipe }
+    }
+
+    fn get_neighbours(&self, grid: &Vec<Vec<Tile>>) -> Vec<Tile> {
+        let mut neighbours = vec![];
+        let grid_height: usize = grid.len() - 1;
+        let grid_width = grid[0].len() - 1;
+
+        match grid[self.position.0][self.position.1].pipe {
+            Pipe::NorthSouth => {
+                // North
+                if self.position.0 > 0 {
+                    if matches!(
+                        grid[self.position.0 - 1][self.position.1].pipe,
+                        Pipe::NorthSouth | Pipe::SouthWest | Pipe::SouthEast | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0 - 1][self.position.1])
+                    }
+                }
+                // South
+                if self.position.0 < grid_height {
+                    if matches!(
+                        grid[self.position.0 + 1][self.position.1].pipe,
+                        Pipe::NorthSouth | Pipe::NorthWest | Pipe::NorthEast | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0 + 1][self.position.1])
+                    }
+                }
+            }
+            Pipe::EastWest => {
+                // East
+                if self.position.1 < grid_width {
+                    if matches!(
+                        grid[self.position.0][self.position.1 + 1].pipe,
+                        Pipe::EastWest | Pipe::NorthWest | Pipe::SouthWest | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0][self.position.1 + 1])
+                    }
+                }
+                // West
+                if self.position.1 > 0 {
+                    if matches!(
+                        grid[self.position.0][self.position.1 - 1].pipe,
+                        Pipe::EastWest | Pipe::NorthEast | Pipe::SouthEast | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0][self.position.1 - 1])
+                    }
+                }
+            }
+            Pipe::NorthEast => {
+                // North
+                if self.position.0 > 0 {
+                    if matches!(
+                        grid[self.position.0 - 1][self.position.1].pipe,
+                        Pipe::NorthSouth | Pipe::SouthWest | Pipe::SouthEast | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0 - 1][self.position.1])
+                    }
+                }
+                // East
+                if self.position.1 < grid_width {
+                    if matches!(
+                        grid[self.position.0][self.position.1 + 1].pipe,
+                        Pipe::EastWest | Pipe::NorthWest | Pipe::SouthWest | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0][self.position.1 + 1])
+                    }
+                }
+            }
+            Pipe::NorthWest => {
+                // North
+                if self.position.0 > 0 {
+                    if matches!(
+                        grid[self.position.0 - 1][self.position.1].pipe,
+                        Pipe::NorthSouth | Pipe::SouthWest | Pipe::SouthEast | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0 - 1][self.position.1])
+                    }
+                }
+
+                // West
+                if self.position.0 > 0 {
+                    if matches!(
+                        grid[self.position.0][self.position.1 - 1].pipe,
+                        Pipe::EastWest | Pipe::NorthEast | Pipe::SouthEast | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0][self.position.1 - 1])
+                    }
+                }
+            }
+            Pipe::SouthWest => {
+                // West
+                if self.position.1 > 0 {
+                    if matches!(
+                        grid[self.position.0][self.position.1 - 1].pipe,
+                        Pipe::EastWest | Pipe::NorthEast | Pipe::SouthEast | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0][self.position.1 - 1])
+                    }
+                }
+                // South
+                if self.position.0 < grid_height {
+                    if matches!(
+                        grid[self.position.0 + 1][self.position.1].pipe,
+                        Pipe::NorthSouth | Pipe::NorthWest | Pipe::NorthEast | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0 + 1][self.position.1])
+                    }
+                }
+            }
+            Pipe::SouthEast => {
+                // East
+                if self.position.1 < grid_width {
+                    if matches!(
+                        grid[self.position.0][self.position.1 + 1].pipe,
+                        Pipe::EastWest | Pipe::NorthWest | Pipe::SouthWest | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0][self.position.1 + 1])
+                    }
+                }
+
+                // South
+                if self.position.0 < grid_height {
+                    if matches!(
+                        grid[self.position.0 + 1][self.position.1].pipe,
+                        Pipe::NorthSouth | Pipe::NorthWest | Pipe::NorthEast | Pipe::Start
+                    ) {
+                        neighbours.push(grid[self.position.0 + 1][self.position.1])
+                    }
+                }
+            }
+            Pipe::Start => {
+                // North
+                if self.position.0 > 0 {
+                    if matches!(
+                        grid[self.position.0 - 1][self.position.1].pipe,
+                        Pipe::NorthSouth | Pipe::SouthEast | Pipe::SouthWest
+                    ) {
+                        neighbours.push(grid[self.position.0 - 1][self.position.1])
+                    }
+                }
+
+                // South
+                if self.position.0 < grid_height {
+                    if matches!(
+                        grid[self.position.0 + 1][self.position.1].pipe,
+                        Pipe::NorthSouth | Pipe::NorthEast | Pipe::NorthWest
+                    ) {
+                        neighbours.push(grid[self.position.0 + 1][self.position.1])
+                    }
+                }
+
+                // West
+                if self.position.1 > 0 {
+                    if matches!(
+                        grid[self.position.0][self.position.1 - 1].pipe,
+                        Pipe::EastWest | Pipe::NorthEast | Pipe::SouthEast
+                    ) {
+                        neighbours.push(grid[self.position.0][self.position.1 - 1])
+                    }
+                }
+
+                // East
+                if self.position.1 < grid_width {
+                    if matches!(
+                        grid[self.position.0][self.position.1 + 1].pipe,
+                        Pipe::EastWest | Pipe::NorthWest | Pipe::SouthWest
+                    ) {
+                        neighbours.push(grid[self.position.0][self.position.1 + 1])
+                    }
+                }
+            }
+            Pipe::Ground => (),
+        }
+
+        neighbours
+    }
+}
+
+fn get_pipe(c: char) -> Pipe {
+    match c {
+        '|' => Pipe::NorthSouth,
+        '-' => Pipe::EastWest,
+        'L' => Pipe::NorthEast,
+        'J' => Pipe::NorthWest,
+        '7' => Pipe::SouthWest,
+        'F' => Pipe::SouthEast,
+        'S' => Pipe::Start,
+        '.' => Pipe::Ground,
+        _ => panic!("Unknown pipe: {}", c),
+    }
+}
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut grid: Vec<Vec<Tile>> = Vec::new();
+    let mut start: Tile = Tile::new((0, 0), Pipe::Ground);
+
+    input.lines().enumerate().for_each(|(r, line)| {
+        let mut row: Vec<Tile> = Vec::new();
+        line.chars().enumerate().for_each(|(c, ch)| {
+            let pipe = get_pipe(ch);
+            let tile = Tile::new((r, c), pipe);
+            if tile.pipe == Pipe::Start {
+                start = tile.clone();
+            }
+            row.push(tile);
+        });
+        grid.push(row);
+    });
+
+    let mut path: HashSet<Tile> = HashSet::new();
+    path.insert(start);
+    let mut to_explore: Vec<Tile> = start.get_neighbours(&grid);
+
+    while to_explore.len() > 0 {
+        let current = to_explore.pop().unwrap();
+        for neighbour in current.get_neighbours(&grid) {
+            if !path.contains(&neighbour) {
+                to_explore.push(neighbour);
+                path.insert(neighbour);
+            }
+        }
+    }
+
+    Some((path.len() / 2) as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut grid: Vec<Vec<Tile>> = Vec::new();
+    let mut start: Tile = Tile::new((0, 0), Pipe::Ground);
+
+    input.lines().enumerate().for_each(|(r, line)| {
+        let mut row: Vec<Tile> = Vec::new();
+        line.chars().enumerate().for_each(|(c, ch)| {
+            let pipe = get_pipe(ch);
+            let tile = Tile::new((r, c), pipe);
+            if tile.pipe == Pipe::Start {
+                start = tile.clone();
+            }
+            row.push(tile);
+        });
+        grid.push(row);
+    });
+
+    let mut path: Vec<Tile> = Vec::new();
+    path.push(start);
+    let mut to_explore: Vec<Tile> = start.get_neighbours(&grid);
+
+    while to_explore.len() > 0 {
+        let current = to_explore.pop().unwrap();
+        for neighbour in current.get_neighbours(&grid) {
+            if !path.contains(&neighbour) {
+                to_explore.push(neighbour);
+                path.push(neighbour);
+            }
+        }
+    }
+
+    println!("{:?}", path);
+
+    //Some(shoelacea(&path).mul(2).sub(path.len() as i32).div(2).add(1) as u32)
+    Some(0)
+}
+
+pub fn shoelacea(tiles: &Vec<Tile>) -> i32 {
+    let len = tiles.len();
+
+    let res = tiles.iter().enumerate().fold(0, |s: i32, (i, t)| {
+        let l = (i + 1) % len;
+
+        s + ((t.position.1 * tiles[l].position.0) as i32)
+            - ((t.position.0 * tiles[l].position.1) as i32)
+    });
+
+    (res / 2) as i32
+}
+
+fn set_start_pipe(start: &Tile, grid: &Vec<Vec<Tile>>) -> Pipe {
+    let neighbours: Vec<Tile> = start.get_neighbours(&grid);
+
+    let north = neighbours
+        .iter()
+        .find(|n| n.position.0 < start.position.0)
+        .is_some();
+
+    let south = neighbours
+        .iter()
+        .find(|n| n.position.0 > start.position.0)
+        .is_some();
+
+    let west = neighbours
+        .iter()
+        .find(|n| n.position.1 < start.position.1)
+        .is_some();
+
+    let east = neighbours
+        .iter()
+        .find(|n| n.position.1 > start.position.1)
+        .is_some();
+
+    match (north, west, south, east) {
+        (true, true, _, _) => Pipe::NorthWest,
+        (true, _, true, _) => Pipe::NorthSouth,
+        (true, _, _, true) => Pipe::NorthEast,
+        (_, true, true, _) => Pipe::SouthWest,
+        (_, _, true, true) => Pipe::SouthEast,
+        (_, true, _, true) => Pipe::EastWest,
+        _ => panic!("No valid tile to replace Start with was found"),
+    }
 }
 
 #[cfg(test)]
--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,11 +1,103 @@
 advent_of_code::solution!(11);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn build_galaxies(input: &str) -> Vec<(usize, usize)> {
+    let mut galaxies: Vec<(usize, usize)> = Vec::new();
+
+    for (i, row) in input.lines().enumerate() {
+        for (j, col) in row.chars().enumerate() {
+            if col.eq(&'#') {
+                galaxies.push((i, j))
+            }
+        }
+    }
+    galaxies
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn extract_empty_rows(mut galaxies: Vec<(usize, usize)>) -> Vec<usize> {
+    let mut empty_rows: Vec<usize> = Vec::new();
+
+    galaxies.sort_by(|a, b| a.0.cmp(&b.0));
+
+    for i in 0..galaxies.len() - 1 {
+        if galaxies[i + 1].0 - galaxies[i].0 > 1 {
+            for j in galaxies[i].0 + 1..galaxies[i + 1].0 {
+                empty_rows.push(j);
+            }
+        }
+    }
+
+    empty_rows
+}
+
+fn extract_empty_cols(mut galaxies: Vec<(usize, usize)>) -> Vec<usize> {
+    let mut empty_cols: Vec<usize> = Vec::new();
+
+    galaxies.sort_by(|a, b| a.1.cmp(&b.1));
+
+    for i in 0..galaxies.len() - 1 {
+        if galaxies[i + 1].1 - galaxies[i].1 > 1 {
+            for j in galaxies[i].1 + 1..galaxies[i + 1].1 {
+                empty_cols.push(j);
+            }
+        }
+    }
+
+    empty_cols
+}
+
+fn expand_galaxies(
+    galaxies: &mut [(usize, usize)],
+    empty_rows: &[usize],
+    empty_cols: &[usize],
+    rate: i32,
+) {
+    for galaxy in galaxies {
+        let num_step_x = empty_rows.iter().filter(|r| **r < galaxy.0).count() as i32;
+        let num_step_y = empty_cols.iter().filter(|c| **c < galaxy.1).count() as i32;
+
+        galaxy.0 += (num_step_x * (rate - 1)) as usize;
+        galaxy.1 += (num_step_y * (rate - 1)) as usize;
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut galaxies: Vec<(usize, usize)> = build_galaxies(input);
+    let empty_rows: Vec<usize> = extract_empty_rows(galaxies.clone());
+    let empty_cols: Vec<usize> = extract_empty_cols(galaxies.clone());
+
+    expand_galaxies(&mut galaxies, &empty_rows, &empty_cols, 2);
+
+    let mut distances_sum: u32 = 0;
+    for i in 0..galaxies.len() {
+        for j in i + 1..galaxies.len() {
+            let distance = (galaxies[i].0 as i32 - galaxies[j].0 as i32).abs()
+                + (galaxies[i].1 as i32 - galaxies[j].1 as i32).abs();
+
+            distances_sum += distance as u32;
+        }
+    }
+
+    Some(distances_sum)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut galaxies: Vec<(usize, usize)> = build_galaxies(input);
+    let empty_rows: Vec<usize> = extract_empty_rows(galaxies.clone());
+    let empty_cols: Vec<usize> = extract_empty_cols(galaxies.clone());
+
+    expand_galaxies(&mut galaxies, &empty_rows, &empty_cols, 1_000_000);
+
+    let mut distances_sum: u64 = 0;
+    for i in 0..galaxies.len() {
+        for j in i + 1..galaxies.len() {
+            let distance = (galaxies[i].0 as i32 - galaxies[j].0 as i32).abs()
+                + (galaxies[i].1 as i32 - galaxies[j].1 as i32).abs();
+
+            distances_sum += distance as u64;
+        }
+    }
+
+    Some(distances_sum)
 }
 
 #[cfg(test)]
